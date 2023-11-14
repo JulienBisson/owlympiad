0d3f7f3ad9067a9cdc10856220376b2f247ebd13
Julien Bisson <julien.bisson2@gmail.com>
2023-11-14 15:18:28 +0100

    commit: redirection connection path

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // Permet d'obtenir l'erreur de connexion s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier nom utilisateur saisi par l'utilisateur
+        $lastUsername = $authenticationUtils->getLAstUsername();
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error'=> $error,
         ]);
     }
 }
